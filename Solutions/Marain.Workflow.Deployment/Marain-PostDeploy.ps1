603c2d217ab0d2089fd2efeca72097c4689964ef
Ian Griffiths <ian@interact-sw.co.uk>
2020-02-21 09:47:23 +0000

    Feature/43 marain deployment (#79)

---
+++ Solutions/Marain.Workflow.Deployment/Marain-PostDeploy.ps1
@@ -0,0 +1,24 @@
+﻿<#
+This is called during Marain.Instance infrastructure deployment after the Marain-ArmDeploy.ps
+script. It is our opportunity to do any deployment work that needs to happen after Azure resources
+have been deployed.
+#>
+
+# Marain.Instance expects us to define just this one function.
+Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
+
+    # Temporary workaround until https://github.com/marain-dotnet/Marain.Instance/issues/12 implemented
+    $ServiceDeploymentContext.MakeAppServiceCommonService("Marain.Workflow.Hack.Engine", "eng")
+
+    Write-Host 'Uploading function code packages'
+
+    $ServiceDeploymentContext.UploadReleaseAssetAsAppServiceSitePackage(
+        "Marain.Workflow.Api.MessageProcessingHost.zip",
+        $ServiceDeploymentContext.AppName + "mi"
+    )
+    $ServiceDeploymentContext.UploadReleaseAssetAsAppServiceSitePackage(
+        "Marain.Workflow.Api.EngineHost.zip",
+        $ServiceDeploymentContext.AppName + "eng"
+    )
+
+}
---
+++ Solutions/Marain.Workflow.Deployment/Marain-PreDeploy.ps1
@@ -0,0 +1,25 @@
+﻿<#
+This is called during Marain.Instance infrastructure deployment prior to the Marain-ArmDeploy.ps
+script. It is our opportunity to perform initialization that needs to complete before any Azure
+resources are created.
+
+We create the Azure AD Applications that the Workflow functions will use to authenticate incoming
+requests. (Currently, this application is used with Azure Easy Auth, but the service could also
+use it directly.)
+
+#>
+
+# Marain.Instance expects us to define just this one function.
+Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
+
+    $EngineApp = $ServiceDeploymentContext.DefineAzureAdAppForAppService("eng")
+    $MessageIngestionApp = $ServiceDeploymentContext.DefineAzureAdAppForAppService("mi")
+
+    $EngineControllerAppRoleId = "37a5c4e2-38e2-47de-8576-6b1ce7cc0ca2"
+    $EngineApp.EnsureAppRolesContain(
+        $EngineControllerAppRoleId,
+        "Workflow Engine controller",
+        "Able to create workflow instances, and send triggers",
+        "WorkflowController",
+        ("User", "Application"))
+}
