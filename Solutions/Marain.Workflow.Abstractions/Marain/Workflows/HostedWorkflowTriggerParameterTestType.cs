2c6b1e2d28f2fef78860751ccd084b1c607d82fb
Jonathan George <jon@jonathangeorge.co.uk>
2020-05-11 00:20:24 +0100

    Added new condition for hosted workflow triggers (#131)
    * Added new condition for hosted workflow triggers.
    
    * Naming tweaks. Also disabled DF0108 rule temporarily as it gives an incorrect warning when using tuples as arguments to activities.

---
+++ Solutions/Marain.Workflow.Abstractions/Marain/Workflows/HostedWorkflowTriggerParameterTestType.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="HostedWorkflowTriggerParameterTestType.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.Workflows
+{
+    /// <summary>
+    /// Types of test for the <see cref="HostedWorkflowTriggerParameterCondition"/>.
+    /// </summary>
+    public enum HostedWorkflowTriggerParameterTestType
+    {
+        /// <summary>
+        /// The parameter value should match the value specified in the condition.
+        /// </summary>
+        Equals,
+
+        /// <summary>
+        /// The parameter value should not match the value specified in the condition.
+        /// </summary>
+        DoesNotEqual,
+
+        /// <summary>
+        /// The specified parameter must be supplied.
+        /// </summary>
+        Exists,
+
+        /// <summary>
+        /// The specified parameter must not be present.
+        /// </summary>
+        DoesNotExist,
+    }
+}
