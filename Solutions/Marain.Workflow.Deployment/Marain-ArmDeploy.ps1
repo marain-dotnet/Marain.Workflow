e5a5c3c1b16053744a2be6e7d10a6a51b579803c
James Dawson <james.dawson@endjin.com>
2020-05-12 22:34:53 +0100

    Updated ARM deployment with service tenant configuration details
    This is currently hard-coded as 'well-known' values, but will need to be more discoverable in future.

--- Solutions/Marain.Workflow.Deployment/Marain-ArmDeploy.ps1
+++ Solutions/Marain.Workflow.Deployment/Marain-ArmDeploy.ps1
@@ -6,6 +6,10 @@ script. It is our opportunity to create Azure resources.
 # Marain.Instance expects us to define just this one function.
 Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
 
+    # TODO: make this discoverable
+    $serviceTenantId = '3633754ac4c9be44b55bfe791b1780f177b464860334774cabb2f9d1b95b0c18'
+    $serviceTenantDisplayName = 'Workflow v1'
+
     [MarainAppService]$TenancyService = $ServiceDeploymentContext.InstanceContext.GetCommonAppService("Marain.Tenancy")
     [MarainAppService]$OperationsService = $ServiceDeploymentContext.InstanceContext.GetCommonAppService("Marain.Tenancy.Operations.Control")
 
@@ -20,6 +24,8 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
         tenancyServiceResourceIdForMsiAuthentication=$TenancyService.AuthAppId
         tenancyServiceBaseUri=$TenancyService.BaseUrl
         appInsightsInstrumentationKey=$ServiceDeploymentContext.InstanceContext.ApplicationInsightsInstrumentationKey
+        marainServiceTenantId=$serviceTenantId
+        marainServiceTenantDisplayName=$serviceTenantDisplayName
     }
     $InstanceResourceGroupName = $InstanceDeploymentContext.MakeResourceGroupName("workflow")
     $DeploymentResult = $ServiceDeploymentContext.InstanceContext.DeployArmTemplate(
