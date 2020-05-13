a7db44acd18f68c0b067759c1e799081bf134ade
Matthew Adams <matthew.adams@endjin.com>
2020-05-13 16:42:30 +0100

    Updated the documentation to help with understanding what the log entry actually is.

--- Solutions/Marain.Workflow.Abstractions/Marain/Workflows/IWorkflowInstanceChangeLogWriter.cs
+++ Solutions/Marain.Workflow.Abstractions/Marain/Workflows/IWorkflowInstanceChangeLogWriter.cs
@@ -14,10 +14,13 @@ namespace Marain.Workflows
         /// <summary>
         /// Creates a workflow instance change record.
         /// </summary>
-        /// <param name="trigger">A snapshot of the trigger that caused the change in the workflow instance, or null if this was an initialization.</param>
-        /// <param name="instance">A snapshot of the workflow instance that has changed.</param>
+        /// <param name="trigger">The trigger that caused the update to the workflow instance, from which to create a snapshot; or null if this was an initialization operation for the workflow instance.</param>
+        /// <param name="instance">The workflow instance in its updated state, from which to create a snapshot.</param>
         /// <param name="partitionKey">The partition key for the record.</param>
         /// <returns>A <see cref="Task"/> that completes once the record has been stored.</returns>
+        /// <remarks>
+        /// <para>The log entry consists of a snapshot of the trigger that caused the state change in the instance, and a snapshot of the workflow instance in that new state.</para>
+        /// </remarks>
         Task CreateLogEntryAsync(IWorkflowTrigger trigger, WorkflowInstance instance, string partitionKey = null);
     }
 }
