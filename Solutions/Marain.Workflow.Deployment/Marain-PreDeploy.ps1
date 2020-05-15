aad50976570d26206ae623a238695490eda6f320
James Dawson <james.dawson@endjin.com>
2020-05-15 09:59:02 +0100

    Note to capture a current state limitation

--- Solutions/Marain.Workflow.Deployment/Marain-PreDeploy.ps1
+++ Solutions/Marain.Workflow.Deployment/Marain-PreDeploy.ps1
@@ -26,6 +26,7 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
     # ensure the service tenancy exists
     $serviceManifest = Join-Path $PSScriptRoot "..\ServiceManifests\WorkflowServiceManifest.jsonc" -Resolve
     try {
+        # TODO: If tenancy wasn't run, we won't currently have the marain cli configuration setup
         $cliOutput = & $ServiceDeploymentContext.InstanceContext.MarainCliPath create-service $serviceManifest
         if ( $LASTEXITCODE -ne 0 -and -not ($cliOutput -imatch 'service tenant.*already exists') ) {
             # TODO: Ignore error when service tenant already exists
