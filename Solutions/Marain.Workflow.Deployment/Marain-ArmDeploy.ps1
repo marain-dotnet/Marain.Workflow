16c46f66f0057a362d8dbb4c4a5750c9f18085c7
James Dawson <james.dawson@endjin.com>
2020-05-22 07:44:22 +0100

    Updated powershell deployment (#151)
    Fixes the deployment project packaging and adds support for configuring the Workflow service tenancy.
    
    It relies on the main deployment in Marain.Instance for ensuring the `marain` .net global tool is available.

--- Solutions/Marain.Workflow.Deployment/Marain-ArmDeploy.ps1
+++ Solutions/Marain.Workflow.Deployment/Marain-ArmDeploy.ps1
@@ -6,6 +6,10 @@ script. It is our opportunity to create Azure resources.
 # Marain.Instance expects us to define just this one function.
 Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
 
+    # TODO: make this discoverable
+    $serviceTenantId = '3633754ac4c9be44b55bfe791b1780f177b464860334774cabb2f9d1b95b0c18'
+    $serviceTenantDisplayName = 'Workflow v1'
+
     [MarainAppService]$TenancyService = $ServiceDeploymentContext.InstanceContext.GetCommonAppService("Marain.Tenancy")
     [MarainAppService]$OperationsService = $ServiceDeploymentContext.InstanceContext.GetCommonAppService("Marain.Tenancy.Operations.Control")
 
@@ -20,6 +24,8 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
         tenancyServiceResourceIdForMsiAuthentication=$TenancyService.AuthAppId
         tenancyServiceBaseUri=$TenancyService.BaseUrl
         appInsightsInstrumentationKey=$ServiceDeploymentContext.InstanceContext.ApplicationInsightsInstrumentationKey
+        marainServiceTenantId=$serviceTenantId
+        marainServiceTenantDisplayName=$serviceTenantDisplayName
     }
     $InstanceResourceGroupName = $InstanceDeploymentContext.MakeResourceGroupName("workflow")
     $DeploymentResult = $ServiceDeploymentContext.InstanceContext.DeployArmTemplate(
@@ -30,4 +36,18 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
 
     $ServiceDeploymentContext.SetAppServiceDetails($DeploymentResult.Outputs.messageIngestionFunctionServicePrincipalId.Value, "mi", $null)
     $ServiceDeploymentContext.SetAppServiceDetails($DeploymentResult.Outputs.engineFunctionServicePrincipalId.Value, "eng", $null)
+
+
+    # ensure the service tenancy exists
+    Write-Host "Ensuring Workflow service tenancy..."
+    $serviceManifest = Join-Path $PSScriptRoot "ServiceManifests\WorkflowServiceManifest.jsonc" -Resolve
+    try {
+        $cliOutput = & $ServiceDeploymentContext.InstanceContext.MarainCliPath create-service $serviceManifest
+        if ( $LASTEXITCODE -ne 0 -and -not ($cliOutput -imatch 'service tenant.*already exists') ) {
+            Write-Error "Error whilst trying to register the Workflow service tenant: ExitCode=$LASTEXITCODE`n$cliOutput"
+        }
+    }
+    catch {
+        throw $_
+    }
 }
