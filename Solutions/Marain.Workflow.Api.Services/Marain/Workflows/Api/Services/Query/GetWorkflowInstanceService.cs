06b650d8c96f87d977a9f4e2d4e2e758198df00a
Jonathan George <jon@jonathangeorge.co.uk>
2020-08-13 15:29:42 +0100

    Implemented get workflow instance endpoint.

--- Solutions/Marain.Workflow.Api.Services/Marain/Workflows/Api/Services/Query/GetWorkflowInstanceService.cs
+++ Solutions/Marain.Workflow.Api.Services/Marain/Workflows/Api/Services/Query/GetWorkflowInstanceService.cs
@@ -4,8 +4,13 @@
 
 namespace Marain.Workflows.Api.Services.Query
 {
+    using System;
     using System.Threading.Tasks;
+    using Corvus.Tenancy;
+    using Marain.Services.Tenancy;
+    using Marain.Workflows.Api.Services.Query.Mappers;
     using Menes;
+    using Menes.Hal;
 
     /// <summary>
     /// Implementation of the getWorkflowInstance operation.
@@ -17,6 +22,29 @@ namespace Marain.Workflows.Api.Services.Query
         /// </summary>
         public const string GetWorkflowInstanceOperationId = "getWorkflowInstance";
 
+        private readonly IMarainServicesTenancy marainServicesTenancy;
+        private readonly ITenantedWorkflowInstanceStoreFactory workflowInstanceStoreFactory;
+        private readonly WorkflowInstanceMapper workflowInstanceMapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetWorkflowService"/> class.
+        /// </summary>
+        /// <param name="workflowInstanceStoreFactory">The workflow store factory.</param>
+        /// <param name="marainServicesTenancy">The tenancy services.</param>
+        /// <param name="workflowInstanceMapper">The workflow mapper.</param>
+        public GetWorkflowInstanceService(
+            ITenantedWorkflowInstanceStoreFactory workflowInstanceStoreFactory,
+            IMarainServicesTenancy marainServicesTenancy,
+            WorkflowInstanceMapper workflowInstanceMapper)
+        {
+            this.marainServicesTenancy = marainServicesTenancy
+                ?? throw new ArgumentNullException(nameof(marainServicesTenancy));
+            this.workflowInstanceStoreFactory = workflowInstanceStoreFactory
+                ?? throw new ArgumentNullException(nameof(workflowInstanceStoreFactory));
+            this.workflowInstanceMapper = workflowInstanceMapper
+                ?? throw new ArgumentNullException(nameof(workflowInstanceMapper));
+        }
+
         /// <summary>
         /// Retrieves a list of states for a workflow, optionally embedding a list of workflow instances in each state.
         /// </summary>
@@ -24,11 +52,22 @@ namespace Marain.Workflows.Api.Services.Query
         /// <param name="workflowInstanceId">The Id of the workflow instance to return.</param>
         /// <returns>An OpenApi response containing the workflow state.</returns>
         [OperationId(GetWorkflowInstanceOperationId)]
-        public Task<OpenApiResult> GetWorkflowInstanceAsync(
+        public async Task<OpenApiResult> GetWorkflowInstanceAsync(
             IOpenApiContext context,
             string workflowInstanceId)
         {
-            return Task.FromResult(this.NotImplementedResult());
+            ITenant tenant = await this.marainServicesTenancy.GetRequestingTenantAsync(context.CurrentTenantId).ConfigureAwait(false);
+            IWorkflowInstanceStore workflowStore = await this.workflowInstanceStoreFactory.GetWorkflowInstanceStoreForTenantAsync(tenant).ConfigureAwait(false);
+            WorkflowInstance workflowInstance = await workflowStore.GetWorkflowInstanceAsync(workflowInstanceId).ConfigureAwait(false);
+            HalDocument response = await this.workflowInstanceMapper.MapAsync(workflowInstance, context).ConfigureAwait(false);
+            OpenApiResult result = this.OkResult(response, "application/json");
+
+            if (!string.IsNullOrEmpty(workflowInstance.ETag))
+            {
+                result.Results.Add("ETag", workflowInstance.ETag);
+            }
+
+            return result;
         }
     }
 }
---
+++ Solutions/Marain.Workflow.Api.Services/Marain/Workflows/Api/Services/Query/Mappers/WorkflowInstanceMapper.cs
@@ -0,0 +1,70 @@
+﻿// <copyright file="WorkflowInstanceMapper.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.Workflows.Api.Services.Query.Mappers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Menes;
+    using Menes.Hal;
+    using Menes.Links;
+
+    /// <summary>
+    /// Maps Workflow Instances to HAL documents.
+    /// </summary>
+    public class WorkflowInstanceMapper : IHalDocumentMapper<WorkflowInstance, IOpenApiContext>
+    {
+        private readonly IHalDocumentFactory halDocumentFactory;
+        private readonly IOpenApiWebLinkResolver openApiWebLinkResolver;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkflowInstanceMapper"/> class.
+        /// </summary>
+        /// <param name="halDocumentFactory">The service provider to construct <see cref="HalDocument"/> instances.</param>
+        /// <param name="openApiWebLinkResolver">The link resolver.</param>
+        public WorkflowInstanceMapper(
+            IHalDocumentFactory halDocumentFactory,
+            IOpenApiWebLinkResolver openApiWebLinkResolver)
+        {
+            this.halDocumentFactory = halDocumentFactory
+                ?? throw new ArgumentNullException(nameof(halDocumentFactory));
+
+            this.openApiWebLinkResolver = openApiWebLinkResolver
+                ?? throw new ArgumentNullException(nameof(openApiWebLinkResolver));
+        }
+
+        /// <inheritdoc/>
+        public void ConfigureLinkMap(IOpenApiLinkOperationMap links)
+        {
+            links.MapByContentTypeAndRelationTypeAndOperationId<WorkflowInstance>(
+                "self",
+                GetWorkflowInstanceService.GetWorkflowInstanceOperationId);
+        }
+
+        /// <inheritdoc/>
+        public ValueTask<HalDocument> MapAsync(WorkflowInstance resource, IOpenApiContext context)
+        {
+            HalDocument resultDoc = this.halDocumentFactory.CreateHalDocumentFrom(new
+            {
+                resource.ContentType,
+                resource.Id,
+                resource.WorkflowId,
+                resource.StateId,
+                resource.Status,
+                resource.Interests,
+                resource.Context,
+            });
+
+            resultDoc.ResolveAndAddByOwnerAndRelationType(
+                this.openApiWebLinkResolver,
+                resource,
+                "self",
+                ("tenantId", context.CurrentTenantId),
+                ("workflowId", resource.WorkflowId),
+                ("workflowInstanceId", resource.Id));
+
+            return new ValueTask<HalDocument>(resultDoc);
+        }
+    }
+}
