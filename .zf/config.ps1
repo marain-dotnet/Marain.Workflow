0a08f0fdbe4c897e923430ed3c35c7b884f99aff
James Dawson <james.dawson@endjin.com>
2025-06-03 22:35:08 +0100

    Migrate build to ZeroFailed tooling and setup GHA build workflow

---
+++ .zf/config.ps1
@@ -0,0 +1,62 @@
+<#
+This example demonstrates a software build process using the 'ZeroFailed.Build.DotNet' extension
+to provide the features needed when building a .NET solutions.
+#>
+
+$zerofailedExtensions = @(
+    @{
+        # References the extension from its GitHub repository. If not already installed, use latest version from 'main' will be downloaded.
+        Name = "ZeroFailed.Build.DotNet"
+        GitRepository = "https://github.com/zerofailed/ZeroFailed.Build.DotNet"
+        GitRef = "main"
+    }
+)
+
+# Load the tasks and process
+. ZeroFailed.tasks -ZfPath $here/.zf
+
+#
+# Build process configuration
+#
+#
+# Build process control options
+#
+$SkipInit = $false
+$SkipVersion = $false
+$SkipBuild = $false
+$CleanBuild = $Clean
+$SkipTest = $false
+$SkipTestReport = $false
+$SkipAnalysis = $false
+$SkipPackage = $false
+
+$SolutionToBuild = (Resolve-Path (Join-Path $here "./Solutions/Marain.Workflow.sln")).Path
+$IncludeAssembliesInCodeCoverage = "Marain.Workflow*"
+$TargetFrameworkMoniker = "net6.0" # prevent GHA trying to run tests under .NET Framework 4.5 - Linux agents now lack mono
+$NugetPublishSource = property ZF_NUGET_PUBLISH_SOURCE "$here/_local-nuget-feed"
+
+# Customise the build process
+task . FullBuild
+
+#
+# Build Process Extensibility Points - uncomment and implement as required
+#
+
+# task RunFirst {}
+# task PreInit {}
+# task PostInit {}
+# task PreVersion {}
+# task PostVersion {}
+# task PreBuild {}
+# task PostBuild {}
+# task PreTest {}
+# task PostTest {}
+# task PreTestReport {}
+# task PostTestReport {}
+# task PreAnalysis {}
+# task PostAnalysis {}
+# task PrePackage {}
+# task PostPackage {}
+# task PrePublish {}
+# task PostPublish {}
+# task RunLast {}
